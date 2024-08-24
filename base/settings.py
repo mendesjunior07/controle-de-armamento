bf2526638ce01a2bab3af34cf8c966156cae7a48
mendesjunior <mendesjunior2007@hotmail.com>
2024-08-24 09:07:20 -0300

    1

--- base/settings.py
+++ base/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'cautelaarmamento', 'templates','cautelaarmamento/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'base.wsgi.application'
 
@@ -127,9 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static','cautelaarmamento\images'),
-]
+STATICFILES_DIRS = []
+
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- cautelaarmamento/views.py
+++ cautelaarmamento/views.py
@@ -38,44 +38,16 @@ def index(request):
 def profile(request):
     return render(request, 'cautelaarmamento/profile.html')
 
-
 @login_required
 def inventario_equipamentos(request):
     print('olá mundo')
     equipamentos = Vtr.objects.all()
     print(equipamentos)
-    return render(request, 'cautelaarmamento/inventario_equipamentos.html', {'equipamentos': equipamentos})
-    
+    # return render(request, 'cautelaarmamento/inventario_equipamentos.html', {'equipamentos': equipamentos})
+    return render(request, 'catalogo_de_equipamento/inventario_equipamentos.html', {'equipamentos': equipamentos})
 
 @login_required
 def registro_view(request):
     # Sua lógica aqui
     return render(request, 'cautelaarmamento/registro.html')
 
-# @login_required
-# def registro_view(request):
-#     # Verifica se a requisição é do tipo POST, o que indica que o formulário foi enviado
-#     if request.method == 'POST':
-#         # Cria uma instância do formulário RegistroForm com os dados enviados pelo usuário
-#         form = RegistroForm(request.POST)
-
-#         # Verifica se os dados do formulário são válidos de acordo com as validações definidas
-#         if form.is_valid():
-#             # Obtém os dados limpos (validados) do campo 'arma' do formulário
-#             arma = form.cleaned_data['arma']
-#             # Obtém os dados limpos (validados) do campo 'equipamento' do formulário
-#             equipamento = form.cleaned_data['equipamento']
-
-#             # Atualiza o campo 'disponivel' da Arma selecionada para False, indicando que a arma não está mais disponível
-#             # Arma.objects.filter(id=arma.id).update(disponivel=False)
-#             # # Atualiza o campo 'disponivel' do Equipamento selecionado para False, indicando que o equipamento não está mais disponível
-#             # Equipamento.objects.filter(id=equipamento.id).update(disponivel=False)
-
-#             # Redireciona o usuário para a página 'registro' após o processamento bem-sucedido do formulário
-#             return redirect('registro')
-#     else:
-#         # Se a requisição não for do tipo POST (provavelmente GET), cria um formulário vazio
-#         form = RegistroForm()
-
-#     # Renderiza a página 'registro.html', passando o formulário como contexto
-#     return render(request, 'registro.html', {'form': form})
